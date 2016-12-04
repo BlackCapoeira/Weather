015a2f926a319769bec5601b76cd27c5acab33c5
BlackCapoeira <cfpm27@hotmail.com>
2016-12-04 17:52:42 +0100

    Xamarin DevDays Hands-on-Lab

---
+++ Weather/ViewModels/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Weather.Models;
+using Weather.Services;
+using Weather.ViewModels.Base;
+
+namespace Weather.ViewModels
+{
+	public class MainViewModel:ViewModelBase
+	{
+		private ObservableCollection<WeatherRoot> _names;
+		private WeatherService _weatherService;
+
+		public ObservableCollection<WeatherRoot> Weather
+		{
+			get { return _names; }
+			set { _names = value; RaisePropertyChanged();}
+		}
+
+		public MainViewModel()
+		{
+			_weatherService = new WeatherService();
+		}
+
+		public async Task LoadWeather()
+		{
+			var city = await _weatherService.GetWeather("Madrid", Units.Metric);
+			var result = await _weatherService.GetForecast(city.CityId);
+
+			Weather = new ObservableCollection<WeatherRoot>();
+
+			foreach (var item in result.Items)
+			{
+				Weather.Add(item);
+			}
+		}
+	}
+}
---
+++ Weather/Views/MainView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Weather.Models;
+using Weather.Services;
+using Weather.ViewModels;
+using Xamarin.Forms;
+
+namespace Weather.Views
+{
+	public partial class MainView : ContentPage
+	{
+		private MainViewModel _viewModel;
+
+
+		public MainView()
+		{
+			InitializeComponent();
+
+			_viewModel = new MainViewModel();
+			BindingContext = _viewModel;
+
+		}
+
+		protected override async void OnAppearing()
+		{
+			base.OnAppearing();
+
+			await _viewModel.LoadWeather();
+		}
+}
+}
